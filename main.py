82d2f0a6084baf9d5e02b54f3d0a48df9dfb9bcb
AviArta <buzlova.a@yandex.ru>
2023-10-01 16:51:31 +0300

    + try/exept

--- main.py
+++ main.py
@@ -7,40 +7,47 @@ def search_change_files(directory, first_data, new_data):
     и делает в них замену одной подстроки на другую, перезаписывая файл.
     Искомая и заменяющая подстроки передаются в параметрах данной функции."""
 
-    for entry in os.scandir(directory):
-        if entry.is_file() and entry.name.endswith('.json'):
-            print(f'Путь: {entry.path}. Имя файла: {entry.name}')  # нашли каждый файл .json
+    try:
+        for entry in os.scandir(directory):
+            if entry.is_file() and entry.name.endswith('.json'):
+                print(f'Путь: {entry.path}. Имя файла: {entry.name}')  # нашли каждый файл .json
 
-            # читаем каждый файл, если найдены нужные подстроки, изменяем их:
-            with open(entry.name, 'r', encoding='utf-8') as json_file:
-                lines = json_file.readlines()
-                copy_lines, index_list = [], []
-                counter = -1
+                # читаем каждый файл, если найдены нужные подстроки, изменяем их:
+                with open(entry.path, 'r', encoding='utf-8') as json_file:
+                    lines = json_file.readlines()
+                    copy_lines, index_list = [], []
+                    counter = -1
 
-                # поиск строк с нужными подстроками:
-                for line in lines:
-                    line = line.strip()  # строка без \n
-                    counter += 1
+                    # поиск строк с нужными подстроками:
+                    for line in lines:
+                        line = line.strip()  # строка без \n
+                        counter += 1
 
-                    if first_data in line:
-                        print(f'Подстрока "{first_data}" найдена в строке {line}.')
-                        index_list.append(counter)   # список индексов строк, где есть нужная подстрока
-                    copy_lines.append(line)  # список всех строк без \n
+                        if first_data in line:
+                            print(f'Подстрока "{first_data}" найдена в строке {line}.')
+                            index_list.append(counter)   # список индексов строк, где есть нужная подстрока
+                        copy_lines.append(line)  # список всех строк без \n
 
-                # изменение строк:
-                for ind in index_list:
-                    new_str = copy_lines[ind].replace(first_data, new_data)  # изменённая строка без лишних символов
-                    copy_lines[ind] = new_str
-                print(f'Изменения в файле {entry.name} сделаны в {len(index_list)} стр.')
-                # print('Результирующий список строк:', copy_lines)
+                    # изменение строк:
+                    for ind in index_list:
+                        new_str = copy_lines[ind].replace(first_data, new_data)  # изменённая строка без лишних символов
+                        copy_lines[ind] = new_str
+                    print(f'Изменения в файле {entry.name} сделаны в {len(index_list)} стр.')
+                    # print('Результирующий список строк:', copy_lines)
 
-            # построчно перезаписываем файл с изменениями:
-            with open(entry.name, 'w', encoding='utf-8') as json_file:
-                json_file.writelines('%s\n' % line for line in copy_lines)  # каждый элемент списка с новой строки
+                # построчно перезаписываем файл с изменениями:
+                with open(entry.path, 'w', encoding='utf-8') as json_file:
+                    json_file.writelines('%s\n' % line for line in copy_lines)  # каждый элемент списка с новой строки
+
+    except FileNotFoundError:
+        print('По указанному пути файл не найден.')
+
+    except:
+        print('Ошибка при работе с файлом.')
 
 
 if __name__ == '__main__':
-    search_change_files('C:/Users/kuvsh/Desktop/Стажировка', 'second', '!NEW!')
+    search_change_files('C:/Users/kuvsh/Desktop/Стажировка/Dir_1', 'second', 'NEW')
 
 
 # ------- намётки с regex
