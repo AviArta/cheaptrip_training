ef4c1b909b550fa56864271361f0c7e9501d43ec
AviArta <buzlova.a@yandex.ru>
2023-10-12 18:13:48 +0300

    Add typing

--- main_pathlib.py
+++ main_pathlib.py
@@ -2,10 +2,11 @@ import pathlib
 import json
 import logging
 
-logging.basicConfig(level='ERROR', filename='logs.log')
+logging.basicConfig(level='ERROR', filename='logs.log', filemode='a',
+                    format='%(asctime)s %(levelname)s %(message)s')
 logger = logging.getLogger()
 
-def search_change_files(directory, first_data, new_data, key='images'):
+def search_change_files(directory: str, first_data: str, new_data: str, key: str='images') -> list:
 
     ''' The function executes the search of files ".json" on the specified path 
     inside the all tree, modifies substrings of an element by key in the found files.
@@ -28,11 +29,11 @@ def search_change_files(directory, first_data, new_data, key='images'):
                 if isinstance(lines[k], list): 
                     # changing substring 'first_data' to substring 'new_data':
                     lines[k][0] = lines[k][0].replace(first_data, new_data)
-            #logger.error(f'new lines: {lines}')
+            logger.error(f'new lines: {lines}')
             
             # recording modified data:
             with open(entry, 'w', encoding='UTF-8') as input_file:
-                json.dump(lines, input_file, indent=4)  
+                json.dump(lines, input_file, indent=4)  # result_dict
             logger.error(f'File {entry.name} changed.')
 
         except FileNotFoundError:
@@ -41,10 +42,12 @@ def search_change_files(directory, first_data, new_data, key='images'):
         except json.JSONDecodeError:
             logger.error(f'File {entry.name} is empty.')
             continue
+        except TypeError:
+            logger.error('Check the data type in the arguments of the function.')
         except:
             logger.error(f'Error working the file {entry.name}.')
             continue
 
 
 if __name__ == '__main__':
-    search_change_files('C:/Users/kuvsh/Desktop/Стажировка', 'f', 'ch')
+    search_change_files('C:/Users/kuvsh/Desktop/Стажировка — копия', 'f', 'ch')   
