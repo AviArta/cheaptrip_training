46e9f549423dd9f33997d42e58b58eb4d3d6bfeb
AviArta <buzlova.a@yandex.ru>
2023-10-27 15:37:52 +0300

    Add argparse

--- change_files.py
+++ change_files.py
@@ -1,23 +1,29 @@
 import pathlib
 import json
+import argparse
 
 from my_logging import logger
 
 
-def search_change_files(directory: str, first_data: str, new_data: str, key: str):
+parser = argparse.ArgumentParser(description='The function executes the search of files ' 
+    '.json on the specified path inside the all tree, modifies substrings of an element '
+    'by key in the found files.It accepts parameters of path, first_data, new_data, key '
+    'and overwrites files.To run code from the command line you need to enter the required '
+    'parameters')
+parser.add_argument('directory', type=str, help='path to the directory for search json files')
+parser.add_argument('first_data', type=str, help='the substring to be modified in the string')
+parser.add_argument('new_data', type=str, help='the new substring toreplace in the string')
+parser.add_argument('key', default='images', type=str, help='the key whose value needs to be modified')
+args = parser.parse_args()
+
+def search_change_files(directory: str, first_data: str, new_data: str, key: str):  # key: str = 'images'
     ''' The function executes the search of files ".json" on the specified path 
     inside the all tree, modifies substrings of an element by key in the found files.
     It accepts parameters of path, first_data, new_data, key and overwrites files.
        To run code from the command line you need to enter the required parameters:
-    [directory] [first_data] [new_data] [key]
-    [directory] - str, path to the directory
-    [first_data] - str, the substring to be modified in the string
-    [new_data] - str, the new substring toreplace in the string
-    [key] - str, the key whose value needs to be modified
-       For example:
-    python main.py C:/Users/kuvsh/Desktop/Стажировка f ch images
     '''
     file_list = list(pathlib.Path(directory).rglob('*.json'))
+    counter_change_files = 0
 
     for entry in file_list:
         try:
@@ -28,9 +34,11 @@ def search_change_files(directory: str, first_data: str, new_data: str, key: str
             # creating the dictionary with modified substrings by key:
             if isinstance(lines[key], list):
                 lines[key] = [element if type(element) != str else element.replace(first_data, new_data) for element in lines[key]]
+                counter_change_files += 1            
             elif isinstance(lines[key], str):
                 lines[key] = lines[key].replace(first_data, new_data)
-
+                counter_change_files += 1
+            
             # recording modified data:
             with open(entry, 'w', encoding='UTF-8') as input_file:
                 json.dump(lines, input_file, indent=4)
@@ -50,3 +58,7 @@ def search_change_files(directory: str, first_data: str, new_data: str, key: str
         except:
             logger.error(f'Error working the file {entry.name}.')
             continue
+
+    print(f'Количество обработанных файлов: {len(file_list)}')
+    print(f'Успешно изменено файлов: {counter_change_files}, без изменений: {len(file_list) - counter_change_files}')
+    print(f'Выполнение программы завершено.')
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sys
-
-from change_files import search_change_files
+from change_files import search_change_files, args
 
 
 if __name__ == '__main__':
     #search_change_files('C:/Users/kuvsh/Desktop/Стажировка', 'f', 'ch', 'images') 
-    search_change_files(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
-    help(search_change_files)
+    search_change_files(args.directory, args.first_data, args.new_data, args.key)
