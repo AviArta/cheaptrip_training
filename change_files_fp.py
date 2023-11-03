ac84f8344b56afa99e8d4df3044e262f70abb80e
AviArta <buzlova.a@yandex.ru>
2023-11-03 17:32:18 +0300

    Add working time decorator

--- change_files_fp.py
+++ change_files_fp.py
@@ -3,7 +3,10 @@ import json
 
 from my_logging import logger
 from my_parser import args
+from my_decorators import working_time_decorator
 
+
+@working_time_decorator
 def search_change_files(directory: str, first_data: str, new_data: str, key: str='images'):  
     ''' The function executes the search of files ".json" on the specified path 
     inside the all tree, modifies substrings of an element by key in the found files.
@@ -15,9 +18,9 @@ def search_change_files(directory: str, first_data: str, new_data: str, key: str
    
     for one_file in pathlib.Path(directory).rglob('*.json'):
         try:
-            lines = read_file(one_file)
+            start_data = read_file(one_file)
             counter_files += 1
-            result_lines = modify_data(lines, first_data, new_data, key)
+            result_lines = modify_data(start_data, first_data, new_data, key)
             counter_change_files += 1
             overwrite_file(one_file, result_lines)
             
@@ -30,6 +33,7 @@ def search_change_files(directory: str, first_data: str, new_data: str, key: str
     print(f'Number of json files: {counter_files}.', f'Number of modified files: {counter_change_files}.',
            f'The program is completed.', sep='\n')
 
+
 def read_file(one_file: str) -> list: 
     ''' The function read file. It accepts parameter of file path and returns list of data
     from file.
@@ -37,18 +41,18 @@ def read_file(one_file: str) -> list:
     with open(one_file, encoding='UTF-8') as input_file:
         return json.load(input_file)
 
-def modify_data(lines: list, first_data: str, new_data: str, key: str, counter_change_files: int=0) -> list:
+def modify_data(start_data: list, first_substr: str, new_substr: str, key: str, counter_change_files: int=0) -> list:
     ''' The function creats the dictionary with modified value-substrings by key. 
-    It accepts parameters: list, first substring, new substring, key for changing value,
+    It accepts parameters: list with start data, first substring, new substring, key for changing value,
     variable for counting modified files. The function returns modify list.
     '''
-    if isinstance(lines[key], list):
-        lines[key] = [element if type(element) != str else element.replace(first_data, new_data) for element in lines[key]]           
+    if isinstance(start_data[key], list):
+        start_data[key] = [element if type(element) != str else element.replace(first_substr, new_substr) for element in start_data[key]]           
         counter_change_files += 1
-    elif isinstance(lines[key], str):
-        lines[key] = lines[key].replace(first_data, new_data)
+    elif isinstance(start_data[key], str):
+        start_data[key] = start_data[key].replace(first_substr, new_substr)
         counter_change_files += 1
-    return lines
+    return start_data
 
 def overwrite_file(one_file: str, result_lines: list):
     ''' The function overwrites modified data to the file. It accepts file path and 
@@ -60,8 +64,8 @@ def overwrite_file(one_file: str, result_lines: list):
 
 
 if __name__ == '__main__': 
+    #search_change_files(args.directory, args.first_data, args.new_data, args.key)
     search_change_files(args.directory, args.first_data, args.new_data, args.key)
-    
 
 # input in console line: python change_files_fp.py --help
 # input in console line: python change_files.py C:/Users/kuvsh/Desktop/Стажировка f ch -k images
---
+++ my_decorators.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+def working_time_decorator(some_function):
+    
+
+    def new_function(*args, **kwargs):
+        start_time = datetime.now()
+        result = some_function(*args, **kwargs)
+        working_time = round((datetime.now() - start_time).total_seconds(), 4)
+        print(f'Working time of function = {working_time} сек.')
+        return result
+    
+    return new_function
