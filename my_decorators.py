f2107e486dff23a4a352b67f8182a89a270dc8fc
AviArta <buzlova.a@yandex.ru>
2023-11-05 19:53:32 +0300

    Modify time

--- my_decorators.py
+++ my_decorators.py
@@ -6,7 +6,7 @@ def working_time_decorator(some_function):
     def new_function(*args, **kwargs):
         start_time = datetime.now()
         result = some_function(*args, **kwargs)
-        working_time = round((datetime.now() - start_time).total_seconds(), 4)
+        working_time = round((datetime.now() - start_time).total_seconds(), 5)
         print(f"Working time of function '{some_function.__name__}' = {working_time} сек.")
         return result
     
