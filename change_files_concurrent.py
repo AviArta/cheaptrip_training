cc06bba6d838e77deb48d4a7c320e4ba41715d2c
AviArta <buzlova.a@yandex.ru>
2023-11-12 19:21:18 +0300

    Add pydantic

--- change_files_concurrent.py
+++ change_files_concurrent.py
@@ -1,6 +1,8 @@
 from concurrent.futures import ThreadPoolExecutor
 import json
 from pathlib import Path
+from pydantic import BaseModel
+from typing import Union
 
 from my_logging import logger
 from my_parser import args
@@ -23,8 +25,9 @@ def search_change_files(directory: str, first_substr: str, new_substr: str, key:
             counter_files += 1
             if start_data[key]:
                 start_value = start_data[key]
-                start_data[key] = modify_data(start_value, first_substr, new_substr, key)
-                #overwrite_file(one_file, start_data)
+                modify_d = Modify_Data(st_val=start_value, f_substr=first_substr, n_substr=new_substr, k=key)
+                start_data[key] = modify_d.modify_data(start_value, first_substr, new_substr, key)
+                overwrite_file(one_file, start_data)
                 counter_change_files += 1
             
         except (json.JSONDecodeError, KeyError):
@@ -37,7 +40,7 @@ def search_change_files(directory: str, first_substr: str, new_substr: str, key:
            f'The program is completed.', sep='\n')
 
 
-@working_time_decorator
+#@working_time_decorator
 def read_file(one_file: str) -> list: 
     ''' The function read file. It accepts parameter of file path and returns list of data
     from file.
@@ -46,20 +49,26 @@ def read_file(one_file: str) -> list:
         return json.load(input_file)
 
 
-@working_time_decorator
-def modify_data(start_value: list or str, first_substr: str, new_substr: str, key: str) -> list:
-    ''' The function creats the modified value-substrings by key. 
-    It accepts parameters: value by key, first substring, new substring, key for changing value,
-    variable for counting modified files. The function returns modify list.
-    '''
-    if isinstance(start_value, list):
-        start_value = [element if type(element) != str else element.replace(first_substr, new_substr) for element in start_value]           
-    elif isinstance(start_value, str):
-        start_value = start_value.replace(first_substr, new_substr)
-    return start_value
+class Modify_Data(BaseModel):
+    st_val: Union[str, list]
+    f_substr: str
+    n_substr: str
+    k: str
+
+    #@working_time_decorator
+    def modify_data(self, start_value: list or str, first_substr: str, new_substr: str, key: str) -> list:
+        ''' The function creats the modified value-substrings by key. 
+        It accepts parameters: value by key, first substring, new substring, key for changing value,
+        variable for counting modified files. The function returns modify list.
+        '''
+        if isinstance(start_value, list):
+            start_value = [element if type(element) != str else element.replace(first_substr, new_substr) for element in start_value]           
+        elif isinstance(start_value, str):
+            start_value = start_value.replace(first_substr, new_substr)
+        return start_value
 
 
-@working_time_decorator
+#@working_time_decorator
 def overwrite_file(one_file: str, result_lines: list):
     ''' The function overwrites modified data to the file. It accepts file path and 
     modified list, it doesn't return anything.
